bcbad5c4357d5568348e81e282ae29b3c2e94fb3
Wojciech Andreas Jurczyk <wajurczyk@gmail.com>
2014-01-26 16:12:15 +0100

    packager creates now standalone windows version

--- PackageApp.bat
+++ PackageApp.bat
@@ -3,9 +3,9 @@ set PAUSE_ERRORS=1
 call bat\SetupSDK.bat
 call bat\SetupApplication.bat
 
-set AIR_TARGET=
-::set AIR_TARGET=-captive-runtime
-set OPTIONS=-tsa none
+::set AIR_TARGET=
+set AIR_TARGET=-captive-runtime
+::set OPTIONS=-tsa none
 call bat\Packager.bat
 
 pause
